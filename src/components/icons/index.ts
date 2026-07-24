3d4211c1dd5aa363c3245165a6e43ff22bfeadb0
AntonioLBZ <antoniolozbez@gmail.com>
2026-07-24 10:26:20 +0200

    [feature/planning-list] add bookmark to planning entries

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
+export { BookmarkIcon } from './bookmark-icon';
 export { CheckMarkIcon } from './check-mark-icon';
 export { DismissIcon } from './dismiss-icon';
 export { FilterIcon } from './filter-icon';
